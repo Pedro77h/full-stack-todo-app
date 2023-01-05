462e159c1ee88e4042502ca7ec275fd3a29cefdd
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2023-01-05 01:54:38 -0300

    Full Stack TODO - Branch resolve

---
+++ front-end/src/components/Columm/Columm.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import {
+  border,
+  BorderBottomProps,
+  BorderProps,
+  borderRadius,
+  BorderRadiusProps,
+  color,
+  ColorProps,
+  layout,
+  LayoutProps,
+  space,
+  SpaceProps,
+} from "styled-system";
+
+type ColumnProps = LayoutProps & SpaceProps & ColorProps & BorderRadiusProps & BorderProps;
+
+export const Column = styled.div<ColumnProps>`
+  display: flex;
+  flex-direction: column;
+  ${layout}
+  ${space}
+  ${color}
+  ${borderRadius}
+  ${border}
+`;
