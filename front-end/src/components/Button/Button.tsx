906f90d283e60669561efde18d9801889639cc53
Pedro77h <Pedrohenriquearaujosoares25@gmail.com>
2023-01-04 23:54:20 -0300

    Full Stack TODO - Branch resolve

--- front-end/src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Button = styled.button`
-  padding: 10px 20px;
-  background-color: transparent;
-  cursor: pointer;
-  color: #fff;
-  border-bottom: 2px solid rgba(255, 255, 255 , 0.2);
-`
--- front-end/src/components/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from './Text'
-export * from './Input'
-export * from './Button'
-export * from './Row'
-export * from './Columm'
-export * from './List'
